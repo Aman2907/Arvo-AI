1620efc719e08e4b71d0a8429327508584493a58
Aman Sharma <74008888+Aman2907@users.noreply.github.com>
2026-08-18 07:19:16 +0530

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,6 @@ import { clerkMiddleware , createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/dist/server/web/spec-extension/response";
 import arcjet, { detectBot, shield } from "@arcjet/next";
 
-
  
 const isProtectedRoute = createRouteMatcher([
   "/workspace(.*)",
